e21c8b463d0da29c77e1ad37c2862157c3a18771
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-22 11:32:28 -0500

    add tests for print_texts

---
+++ print_texts_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char const *argv[])
+{
+	print_char('c');
+	print_string("Hola mundo");
+	print_string("Hola mundo 2");
+	print_string("Hola mundo...");
+	return 0;
+}
