8314a8802d40c0cde1c2d872fc2c5b11f1de0482
Mpqg <4534@holbertonschool.com>
2022-03-22 11:23:57 -0500

    Print numbers

--- prints_nums.c
+++ prints_nums.c
@@ -1,15 +1,30 @@
 #include "main.h"
+/**
+ * print_int - print an integer
+ *
+ * @i: integer
+ * Return: Always 0
+ */
 
 int print_int(int i)
 {
-	int i;
 	if (i < 0)
 	{
-		/* asdsdad*/
-		_putchar();
+		return (NULL);
 	}
+	_putchar(i);
 }
-
+/**
+ * print_unsigned - print an unsigned integer
+ *
+ * @i: integer
+ * Return: Always 0
+ */
 int print_unsigned(unsigned int i)
 {
+	if (i < 0)
+	{
+		return (NULL);
+	}
+	_putchar(i);
 }
