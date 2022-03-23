e1227bbe2e8372dd6e86180d6e43309a398e15fe
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-23 16:30:49 -0500

    add nil print

--- _printf.c
+++ _printf.c
@@ -7,4 +7,46 @@
  */
 int _printf(const char *format, ...)
 {
+
+	va_list content;
+	int(printer(va_list));
+	va_arg(content, char *);
+	int i = 0;
+	int length = 0;
+
+	/**
+	 * Validate that text
+	 * provided by the user
+	 * is empty; In the case
+	 * that field is empty should
+	 * return -1
+	 *
+	 */
+
+	/**
+	 * Validate that text
+	 * provided by the user
+	 * starts with '%' and the
+	 * next value is empty;
+	 */
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+
+			printer = get_print(format[i + 1]);
+
+			if (get_print(format[i + 1]))
+			{
+				length += printer(content);
+			}
+			else
+			{
+				_putchar(format[i])
+			}
+		}
+	}
+
+	return (length)
 }
