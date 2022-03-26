13591d0e2bd0c5b55627dab465b6c22b190125d7
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-25 20:22:47 -0500

    fix print empty

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 
 	va_start(content, format);
 
-	if (!format || !*format || (format[0] == '%' && !format[1]))
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
